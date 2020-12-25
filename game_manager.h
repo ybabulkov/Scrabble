61274ee8a39928999bd53cbd7ab4422ce4717913
YoanBabulkov <37039986+ybabulkov@users.noreply.github.com>
2020-12-25 17:45:34 +0200

    Added a comment with information about the file

--- game_manager.h
+++ game_manager.h
@@ -1,4 +1,17 @@
-
+/**
+*
+* Solution to course project # 8
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2020/2021
+*
+* @author Yoan Babulkov
+* @idnumber 62601
+* @compiler VC
+*
+* <header file with helper functions>
+*
+*/
 #ifndef GAME_MANAGER_H
 #define GAME_MANAGER_H
 
